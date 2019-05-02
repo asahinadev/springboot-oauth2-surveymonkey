c8990c69c3f797bdac39bd5c7d7bf5d1d8e08ebe
asahinadev <dev.account.01@mail.mirageworld.jp>
2019-05-02 12:10:13 +0900

    カスタム設定のミニマム化

---
+++ src/main/java/com/example/spring/surveymonkey/config/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.example.spring.surveymonkey.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.example.spring.surveymonkey.oauth.user.SurveymonkeyUser;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig
+		extends WebSecurityConfigurerAdapter {
+
+	@Override
+	public void configure(WebSecurity web)
+			throws Exception {
+		super.configure(web);
+
+		web.ignoring().antMatchers(
+				// webjars
+				"/webjars/**",
+				// CSS ファイル
+				"/css/**",
+				// JavaScriptファイル
+				"/js/**",
+				// 画像ファイル
+				"/img/**",
+				// サウンドファイル
+				"/sound/**",
+				// WEB フォント
+				"/font/**",
+				"/fonts/**",
+				// 外部ライブラリ
+				"/exlib/**"
+		/**/
+		);
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth)
+			throws Exception {
+		super.configure(auth);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http)
+			throws Exception {
+
+		super.configure(http);
+
+		http.formLogin().disable();
+		http.logout().disable();
+		http.httpBasic().disable();
+		http.csrf().disable();
+
+		http.oauth2Login()
+
+				// ユーザー情報エンドポイント
+				.userInfoEndpoint()
+				.customUserType(SurveymonkeyUser.class, "surveymonkey");
+
+	}
+}
